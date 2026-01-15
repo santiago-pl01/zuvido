e182b19a70e8047741422ab3ba11c7b87edf34e3
1sad0ra <isadora.cd13@gmail.com>
2026-01-15 15:07:50 -0300

    validar uma senha

---
+++ zuvido/server/validacao.js
@@ -0,0 +1,12 @@
+import { getAuth, validatePassword } from "firebase/auth";
+
+const status = await validatePassword(getAuth(), passwordFromUser);
+if (!status.isValid) {
+  // Password could not be validated. Use the status to show what
+  // requirements are met and which are missing.
+
+  // If a criterion is undefined, it is not required by policy. If the
+  // criterion is defined but false, it is required but not fulfilled by
+  // the given password. For example:
+  const needsLowerCase = status.containsLowercaseLetter !== true;
+}
